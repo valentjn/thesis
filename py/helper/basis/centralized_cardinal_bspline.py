5e41253aed467eb2d827317ddcc6009e42752dbe
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-03-31 16:41:18 +0200

    Extend centralized card. B-spl. for even degrees

--- py/helper/basis/centralized_cardinal_bspline.py
+++ py/helper/basis/centralized_cardinal_bspline.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+import numpy as np
+
 from .non_uniform_bspline import NonUniformBSpline
 
 class CentralizedCardinalBSpline(NonUniformBSpline):
   def __init__(self, p, nu=0):
-    super().__init__(p, list(range(-(p+1)//2, (p+1)//2 + 1)), nu=nu)
+    super().__init__(p, np.linspace(-(p+1)/2, (p+1)/2, p+2), nu=nu)
