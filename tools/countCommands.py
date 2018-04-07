daf97e181f4e00f83c5cfc842fb2fefa90be70b5
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-04-07 16:06:17 +0200

    Only count notation commands in countCommands

--- tools/countCommands.py
+++ tools/countCommands.py
@@ -1,23 +1,50 @@
 #!/usr/bin/python3
 
+import argparse
 import collections
 import os
 import pprint
 import re
 
-if __name__ == "__main__":
-  texPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "tex")
-  isDigit = lambda x: (48 <= ord(x) <= 57)
-  isChapter = lambda x: ((isDigit(x[0]) or
-                         ((x[0] == "a") and isDigit(x[1]))) and x.endswith(".tex"))
+def main():
+  parser = argparse.ArgumentParser(
+    description="Count notation LaTeX commands.")
+  parser.add_argument("--keystrokes", action="store_true",
+                      help="Display number of keystrokes instead "
+                           "(occurences times length).")
+  parser.add_argument("--all-commands", action="store_true",
+                      help="Count all commands, not only the ones "
+                           "defined in notation.tex")
+  args = parser.parse_args()
   
+  texPath = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "..", "tex")
+  
+  if not args.all_commands:
+    notationFile = os.path.join(texPath, "notation.tex")
+    with open(notationFile, "r") as f: tex = f.read()
+    possibleCommands = []
+    #possibleCommands += ([x[1] for x in
+    #    re.findall(r"\\(re)?newcommand\*?\{\\([A-Za-z]+)\}", tex)])
+    possibleCommands += ([x[1] for x in
+        re.findall(r"\\newnotationcommand(\[[0-9]*\])?\{\\([A-Za-z]+)\}", tex)])
+    possibleCommands += ([x[1] for x in
+        re.findall(r"\\newnotationcommandoptarg(\[[^\]]*\])?\{[0-9]+\}"
+               r"\{\\([A-Za-z]+)\}", tex)])
+    possibleCommands.sort()
+  
+  isChapterFilename = lambda x: (
+    (x[0].isdigit() or ((x[0] == "a") and x[1].isdigit())) and
+    x.endswith(".tex"))
   texFiles = sorted([os.path.join(texPath, x) for x in os.listdir(texPath)
-                     if isChapter(x)])
+                     if isChapterFilename(x)])
   commandCounts = {}
   
   for texFile in texFiles:
     with open(texFile, "r") as f: tex = f.read()
-    for command in re.findall(r"\\([A-Za-z]+)", tex):    
+    for command in re.findall(r"\\([A-Za-z]+)", tex):
+      if not args.all_commands:
+        if command not in possibleCommands: continue
       commandCounts[command] = commandCounts.get(command, 0) + 1
   
   notationTexFile = os.path.join(texPath, "notation.tex")
@@ -27,7 +54,16 @@ if __name__ == "__main__":
     if command not in notationTex:
       del commandCounts[command]
   
-  commandKeystrokes = {x : commandCounts[x] * len(x) for x in commandCounts}
-  commandKeystrokes = collections.OrderedDict(sorted(commandKeystrokes.items(),
-                                                     key=lambda x: -x[1]))
-  pprint.pprint(commandKeystrokes)
+  if args.keystrokes:
+    output = {x : commandCounts[x] * len(x) for x in commandCounts}
+  else:
+    output = commandCounts
+  
+  output = collections.OrderedDict(sorted(
+    output.items(), key=lambda x: -x[1]))
+  pprint.pprint(output)
+
+
+
+if __name__ == "__main__":
+  main()
