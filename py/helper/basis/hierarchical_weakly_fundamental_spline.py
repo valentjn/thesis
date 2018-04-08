Fix wrong derivative of level 0 of hier. WFS

py/helper/basis/hierarchical_weakly_fundamental_spline.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import numpy as np
+
 from .weakly_fundamental_spline import WeaklyFundamentalSpline
 from .hierarchical_basis_from_parent_function import HierarchicalBasisFromParentFunction
 
@@ -9,5 +11,13 @@ class HierarchicalWeaklyFundamentalSpline(HierarchicalBasisFromParentFunction):
     self.p = p
   
   def evaluate(self, l, i, xx):
-    return (super().evaluate(l, i, xx) if l >= 1 else
-            i * xx + (1 - i) * (1 - xx))
+    if l >= 1:
+      yy = super().evaluate(l, i, xx)
+    elif self.nu == 0:
+      yy = i * xx + (1 - i) * (1 - xx)
+    elif self.nu == 1:
+      yy = (2*i-1) * np.ones_like(xx)
+    else:
+      yy = np.zeros_like(xx)
+    
+    return yy