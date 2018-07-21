c523c38731a9ade4ff4c1bc41328765efaa3829c
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-07-21 23:41:20 +0200

    Fix wrong definition of weakly fund. NAK B-splines

--- py/helper/basis/nodal_weakly_fundamental_transformed.py
+++ py/helper/basis/nodal_weakly_fundamental_transformed.py
@@ -6,6 +6,7 @@ import helper.function
 import helper.grid
 
 from .hierarchical_fundamental_transformed import HierarchicalFundamentalTransformed
+from .hierarchical_notaknot_bspline import HierarchicalNotAKnotBSpline
 
 class NodalWeaklyFundamentalTransformed(HierarchicalFundamentalTransformed):
   def __init__(self, basis, evaluationBasis=None):
@@ -15,7 +16,17 @@ class NodalWeaklyFundamentalTransformed(HierarchicalFundamentalTransformed):
     hInv = 2**l
     p = self.basis.p
     
+    # without the hack, this produces incorrect results for NAK B-splines
+    # the B-splines of index 3 and hInv-3 don't vanish at 0 and 1;
+    # this is just a dirty hack which should fix the figure in
+    # hierarchicalBasis, not designed for general degrees
+    dirtyHackEnabled = (
+        isinstance(self.basis, HierarchicalNotAKnotBSpline) and (p == 3))
+    
     iLeft, iRight = i-(p-1)//2, i+(p-1)//2
+    if dirtyHackEnabled:
+      if i == 3:        iLeft -= 1
+      if i == hInv - 3: iRight += 1
     omitLeft, omitRight = max(-iLeft, 0), max(iRight - hInv, 0)
     
     iLeft += omitLeft
@@ -23,6 +34,9 @@ class NodalWeaklyFundamentalTransformed(HierarchicalFundamentalTransformed):
     N = iRight - iLeft + 1
     
     x = np.linspace(i-self.p+2, i+self.p-2, N-1) / hInv
+    if dirtyHackEnabled:
+      if i == 3:        x = np.linspace(0, i+self.p-2, N-1) / hInv
+      if i == hInv - 3: x = np.linspace(i-self.p+2, hInv, N-1) / hInv
     x = (x[omitLeft:-omitRight] if omitRight > 0 else
          x[omitLeft:])
     
