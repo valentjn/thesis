0f27ac8699191aba155c3b25d60d1d86aef7c075
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-08-25 10:59:22 +0200

    Add legend for adaptive case in resultsFuzzyLegend

--- gfx/py/resultsFuzzyLegend.py
+++ gfx/py/resultsFuzzyLegend.py
@@ -7,10 +7,8 @@ import helper.plot
 
 
 lineNames = ["B-spl. surrogate", "Linear surrogate"]
-markerStyles = [".", "^",  "v"]
-lineStyles   = ["-", "--", ":"]
-
-
+markerStyles = [".", "^"]
+lineStyles   = ["-", "--"]
 
 fig = Figure.create(figsize=(5, 2))
 ax = fig.gca()
@@ -20,7 +18,7 @@ functionNames = ["Bra02", "GoP", "Sch06", "Ack", "Alp02", "Sch22"]
 lines = [{
     "label"  : r"\rlap{{{}}}".format(lineNames[r]),
     "marker" : markerStyles[r],
-    "ms"     : (6 if r == 0 else 3),
+    "ms"     : (6 if markerStyles[r] == "." else 3),
     "ls"     : lineStyles[r],
     "color"  : "k",
   } for r in range(len(lineNames))]
@@ -35,6 +33,44 @@ helper.plot.addCustomLegend(ax, (
   } for r in range(len(functionNames))] +
   lines
 ), ncol=6, loc="upper center", outside=True)
+
+ax.set_axis_off()
+
+fig.save()
+
+
+
+lineNames = ["B-spl. (adap.)", "Linear (adap.)",
+             "B-spl. (reg.)",  "Linear (reg.)"]
+markerStyles = [".", "^",  ".", "^"]
+lineStyles   = ["-", "--", "-", "--"]
+faceColors = ["k", "k", "none", "none"]
+
+fig = Figure.create(figsize=(6, 2))
+ax = fig.gca()
+
+functionNames = ["Ack", "Alp02", "Sch22"]
+
+lines = [{
+    "label"  : (r"\rlap{{{}}}".format(lineNames[r]) if r == 0 else
+                lineNames[r]),
+    "marker" : markerStyles[r],
+    "ms"     : (1 if r < 2 else 2) * (6 if markerStyles[r] == "." else 3),
+    "ls"     : lineStyles[r],
+    "color"  : helper.plot.mixColors("k", (1 if r < 2 else 0.5),
+                   helper.plot.mixColors("mittelblau", 0.1)),
+    "mfc"    : faceColors[r],
+  } for r in range(len(lineNames))]
+lines = lines[:1] + [None] + lines[1:]
+
+helper.plot.addCustomLegend(ax, (
+  [{
+    "label"  : functionNames[r],
+    "ls"     : "-",
+    "color"  : "C{}".format(r+3),
+  } for r in range(len(functionNames))] + (2 * [None]) +
+  lines
+), ncol=5, loc="upper center", outside=True)
 
 ax.set_axis_off()
 
