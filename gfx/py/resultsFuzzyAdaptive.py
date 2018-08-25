75373645bed3450d0ff229941ccca165a453f326
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-08-25 10:59:01 +0200

    Include regular results in resultsFuzzyAdaptive

--- gfx/py/resultsFuzzyAdaptive.py
+++ gfx/py/resultsFuzzyAdaptive.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# number of output figures = 4
+# number of output figures = 3
 # dependencies = cpp/optimizeFuzzy
 
 import functools
@@ -16,10 +16,9 @@ import helperFuzzy
 
 
 def processParameterCombination(
-    gridType, gridGenerationType, n, p, gamma, m, inputIndex,
-    parameterCombination):
-  (fStr, d, color), N, seed = parameterCombination
-  if N == 0: gridGenerationType = "exact"
+    gridType, p, gamma, m, inputIndex, parameterCombination):
+  (fStr, d, color), gridGenerationType, N, n, seed = parameterCombination
+  if (N == 0) and (n == 0): gridGenerationType = "exact"
   return helperFuzzy.optimizeFuzzy(
       fStr, d, gridType, gridGenerationType,
       N, p, gamma, n, m, inputIndex, seed)
@@ -28,8 +27,6 @@ def processParameterCombination(
 
 def main():
   gridType = "modifiedNotAKnotBSpline"
-  gridGenerationType = "ritterNovak"
-  n = 0
   p = 3
   gamma = 0.9
   m = 100
@@ -38,46 +35,108 @@ def main():
   NMin = 64
   NMax = 20000
   fss = [
-    [("branin02", 2, "C0"), ("goldsteinPrice", 2, "C1"),
-     ("schwefel06", 2, "C2"), ("ackley", 2, "C3"),
-     ("alpine02", 2, "C4"), ("schwefel22", 2, "C5")],
-    #[("ackley", 3, "C3"), ("alpine02", 3, "C4"),
-    # ("schwefel22", 3, "C5")],
-    #[("ackley", 4, "C3"), ("alpine02", 4, "C4"),
-    # ("schwefel22", 4, "C5")],
-    #[("ackley", 6, "C3"), ("alpine02", 6, "C4"),
-    # ("schwefel22", 6, "C5")],
+    #[("branin02", 2, "C0"), ("goldsteinPrice", 2, "C1"),
+    # ("schwefel06", 2, "C2"), ("ackley", 2, "C3"),
+    # ("alpine02", 2, "C4"), ("schwefel22", 2, "C5")],
+    [("ackley", 3, "C3"), ("alpine02", 3, "C4"),
+     ("schwefel22", 3, "C5")],
+    [("ackley", 4, "C3"), ("alpine02", 4, "C4"),
+     ("schwefel22", 4, "C5")],
+    [("ackley", 6, "C3"), ("alpine02", 6, "C4"),
+     ("schwefel22", 6, "C5")],
   ]
   lineStyles = [".-", "^--", "v:"]
   seeds = [1]
   
   Ns = [0] + [int(x) for x in 2**np.arange(np.log2(NMin), np.log2(NMax))]
-  parameterCombinations = [[fTuple, N, seed] for fs in fss
-                           for fTuple in fs for N in Ns for seed in seeds]
+  parameterCombinationsAdaptive = [[fTuple, "ritterNovak", N, 0, seed]
+                                   for fs in fss
+                                   for fTuple in fs
+                                   for N in Ns
+                                   for seed in seeds]
   
+  ds = set([d for fs in fss for _, d, _ in fs])
+  nMaxs = {d : helperFuzzy.getMaximalRegularLevel(d, gridType, NMax)
+           for d in ds}
+  parameterCombinationsRegular = [[fTuple, "regular", 0, n, seed]
+      for fs in fss
+      for fTuple in fs
+      for n in ([0] + list(range(1, nMaxs[fTuple[1]]+1)))
+      for seed in seeds]
+  
+  parameterCombinations = (parameterCombinationsAdaptive +
+                           parameterCombinationsRegular)
   print(parameterCombinations)
   
   with multiprocessing.Pool() as pool:
     results = pool.map(functools.partial(
         processParameterCombination,
-        gridType, gridGenerationType, n, p, gamma, m, inputIndex),
+        gridType, p, gamma, m, inputIndex),
         parameterCombinations)
   
+  resultsAdaptive = results[:len(parameterCombinationsAdaptive)]
+  resultsRegular  = results[len(parameterCombinationsAdaptive):]
   
   
-  j = 0
+  
+  j, k = 0, 0
   
   for q, fs in enumerate(fss):
-    figsize = ((1.83, 2.5) if q == 0 else (1.76, 2.5))
+    figsize = ((2.25, 2.45) if q == 0 else (2.23, 2.45))
     fig = Figure.create(figsize=figsize)
     ax = fig.gca()
     
     yAll = np.array([])
     
-    for fStr, d, color in fs:
-      values = []
+    for fStr, d, colorAdaptive in fs:
+      valuesAdaptive, valuesRegular = [], []
+      colorRegular = helper.plot.mixColors(
+          colorAdaptive, 0.5, helper.plot.mixColors("mittelblau", 0.1))
       xDataExacts, alphaDataExacts = {}, {}
+      
       nMax = nMaxs[d]
+      ns = [0] + list(range(1, nMax+1))
+      
+      for n in ns:
+        curValues = [[], []]
+        
+        for seed in seeds:
+          if n == 0:
+            xDataExacts[seed]     = (
+                resultsRegular[k]["objectiveFunction"]["yFuzzyXData"])
+            alphaDataExacts[seed] = (
+                resultsRegular[k]["objectiveFunction"]["yFuzzyAlphaData"])
+          else:
+            for i, s in enumerate(["smoothInterpolant", "linearInterpolant"]):
+              xDataAppr     = resultsRegular[k][s]["yFuzzyXData"]
+              alphaDataAppr = resultsRegular[k][s]["yFuzzyAlphaData"]
+              error = helperFuzzy.calculateRelativeL2FuzzyError(
+                  xDataExacts[seed], alphaDataExacts[seed],
+                  xDataAppr, alphaDataAppr)
+              curValues[i].append(error)
+          
+          k += 1
+        
+        if n > 0:
+          curValues = [np.mean(x) for x in curValues]
+          valuesRegular.append(curValues)
+      
+      xRegular = [helperFuzzy.getGridSize(d, gridType, n) for n in ns[1:]]
+      ysRegular = np.array(list(zip(*valuesRegular)))
+      ysRegular[ysRegular < 1e-16] = 1e-16
+      print(d, fStr, xRegular, ysRegular)
+      
+      for yRegular, lineStyle in zip(ysRegular, lineStyles):
+        markerSize = (6 if lineStyle[0] == "." else 3)
+        ax.plot(xRegular, yRegular, lineStyle,
+                ms=2*markerSize, mfc="none", color=colorRegular)
+    
+    
+    
+    for fStr, d, colorAdaptive in fs:
+      valuesAdaptive, valuesRegular = [], []
+      colorRegular = helper.plot.mixColors(colorAdaptive, 0.5)
+      xDataExacts, alphaDataExacts = {}, {}
       
       for N in Ns:
         curValues = [[], []]
@@ -85,37 +144,44 @@ def main():
         for seed in seeds:
           if N == 0:
             xDataExacts[seed]     = (
-                results[j]["objectiveFunction"]["yFuzzyXData"])
+                resultsAdaptive[j]["objectiveFunction"]["yFuzzyXData"])
             alphaDataExacts[seed] = (
-                results[j]["objectiveFunction"]["yFuzzyAlphaData"])
+                resultsAdaptive[j]["objectiveFunction"]["yFuzzyAlphaData"])
           else:
-            for k, s in enumerate(["smoothInterpolant", "linearInterpolant"]):
-              xDataAppr     = results[j][s]["yFuzzyXData"]
-              alphaDataAppr = results[j][s]["yFuzzyAlphaData"]
+            for i, s in enumerate(["smoothInterpolant", "linearInterpolant"]):
+              xDataAppr     = resultsAdaptive[j][s]["yFuzzyXData"]
+              alphaDataAppr = resultsAdaptive[j][s]["yFuzzyAlphaData"]
               error = helperFuzzy.calculateRelativeL2FuzzyError(
                   xDataExacts[seed], alphaDataExacts[seed],
                   xDataAppr, alphaDataAppr)
-              curValues[k].append(error)
+              curValues[i].append(error)
           
           j += 1
         
         if N > 0:
           curValues = [np.mean(x) for x in curValues]
-          values.append(curValues)
+          valuesAdaptive.append(curValues)
       
-      x = Ns[1:]
-      ys = np.array(list(zip(*values)))
-      ys[ys < 1e-16] = 1e-16
+      xAdaptive = Ns[1:]
+      ysAdaptive = np.array(list(zip(*valuesAdaptive)))
+      ysAdaptive[ysAdaptive < 1e-16] = 1e-16
+      print(d, fStr, xAdaptive, ysAdaptive)
       
-      for y, lineStyle in zip(ys, lineStyles):
+      for yAdaptive, lineStyle in zip(ysAdaptive, lineStyles):
         markerSize = (6 if lineStyle[0] == "." else 3)
-        ax.plot(x, y, lineStyle, color=color, ms=markerSize, clip_on=False)
-        yAll = np.hstack((yAll, y))
+        ax.plot(xAdaptive, yAdaptive, lineStyle, clip_on=False,
+                ms=markerSize, color=colorAdaptive)
+        yAll = np.hstack((yAll, yAdaptive))
     
+    
+    
+    ax.set_xscale("log")
     ax.set_yscale("log")
     
-    #ax.set_xticks([2, 4, 6, 8, 10])
-    ax.set_xlim(x[0], x[-1])
+    ax.set_xticks([1e2, 1e3, 1e4])
+    #x = np.hstack((xAdaptive, xRegular))
+    x = xAdaptive
+    ax.set_xlim(np.amin(x), np.amax(x))
     
     ax.set_ylim(np.amin(yAll), np.amax(yAll))
     ytl = ["$10^{{{}}}$".format(int(np.log10(y))) for y in ax.get_yticks()]
