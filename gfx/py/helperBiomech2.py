9e67bac9e669ce4a78b1078274d1edb8c6156b1b
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-09-25 16:53:09 +0200

    Cache results in helperBiomech2

--- gfx/py/helperBiomech2.py
+++ gfx/py/helperBiomech2.py
@@ -2,14 +2,30 @@
 
 import numpy as np
 
+import helper.grid
+import helper.hpc
 
 
-def matrix2str(XX):
-  XX = np.array(XX)
-  return "{} {} {}".format(
-      *XX.shape, " ".join([str(x) for x in XX.flatten().tolist()]))
 
-def applyBiomech2(action, gridType, basisType, p, forceLoad, XX):
+def applyBiomech2Scattered(action, gridType, basisType, p, forceLoad, XX):
+  XX = tuple(tuple(xx) for xx in np.array(XX))
+  return _applyBiomech2(action, gridType, basisType, p, forceLoad, XX)
+
+@helper.hpc.cacheToFile
+def _applyBiomech2Scattered(action, gridType, basisType, p, forceLoad, XX):
+  return _applyBiomech2(action, gridType, basisType, p, forceLoad, XX)
+
+@helper.hpc.cacheToFile
+def applyBiomech2MeshGrid(action, gridType, basisType, p, forceLoad, nn):
+  _, _, XX = helper.grid.generateMeshGrid(nn)
+  return _applyBiomech2(action, gridType, basisType, p, forceLoad, XX)
+
+@helper.hpc.cacheToFile
+def applyBiomech2MonteCarlo(action, gridType, basisType, p, forceLoad, NN):
+  XX = getMonteCarloPoints(NN)
+  return _applyBiomech2(action, gridType, basisType, p, forceLoad, XX)
+
+def _applyBiomech2(action, gridType, basisType, p, forceLoad, XX):
   import json
   import os
   import subprocess
@@ -20,13 +36,27 @@ def applyBiomech2(action, gridType, basisType, p, forceLoad, XX):
           "basisType={}".format(basisType), "p={}".format(p),
           "forceLoad={}".format(forceLoad)]
   stdin = (matrix2str(XX).encode() if XX is not None else None)
+  
   process = subprocess.Popen(
       args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
       stderr=subprocess.DEVNULL)
   stdout, _ = process.communicate(input=stdin)
+  
   assert process.returncode == 0, "applyBiomech2 terminated unsuccessfully."
   result = np.array(json.loads(stdout.decode()))
+  
   return result
+
+def matrix2str(XX):
+  XX = np.array(XX)
+  return "{} {} {}".format(
+      *XX.shape, " ".join([str(x) for x in XX.flatten().tolist()]))
+
+def getMonteCarloPoints(NN):
+  np.random.seed(342)
+  d = 2
+  XX = np.random.rand(NN, d)
+  return XX
 
 def computeRelativeL2Error(YYExact, YYAppr):
   NN = YYExact.shape[0]
