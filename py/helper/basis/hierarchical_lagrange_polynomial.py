863eb839fac3bd0f53f7da4bb2c61d5485b7fa0d
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-03-29 09:20:24 +0100

    Add derivatives to Lagrange polynomials

--- py/helper/basis/hierarchical_lagrange_polynomial.py
+++ py/helper/basis/hierarchical_lagrange_polynomial.py
@@ -9,7 +9,7 @@ from .hierarchical_basis import HierarchicalBasis
 class HierarchicalLagrangePolynomial(HierarchicalBasis):
   def __init__(self, nu=0):
     super().__init__(nu=nu)
-    assert nu == 0
+    assert nu in [0, 1, 2]
   
   def getCoordinates(self, l, I):
     X = helper.grid.getCoordinates(l, I)
@@ -18,10 +18,38 @@ class HierarchicalLagrangePolynomial(HierarchicalBasis):
   def evaluate(self, l, i, xx):
     I = helper.grid.getNodalIndices(l)
     X = self.getCoordinates(l, I)
-    yy = np.ones_like(xx)
     
-    for ip in I:
-      if ip != i: yy *= (xx - X[ip]) / (X[i] - X[ip])
+    if self.nu == 0:
+      yy = np.ones_like(xx)
+      for i2 in I:
+        if i2 != i: yy *= (xx - X[i2]) / (X[i] - X[i2])
+    elif self.nu == 1:
+      yy        = np.zeros_like(xx)
+      yyProduct = np.ones_like(xx)
+      for i2 in I:
+        if i2 == i: continue
+        yyProduct.fill(1)
+        for i3 in I:
+          if i3 in (i, i2): continue
+          yyProduct *= (xx - X[i3]) / (X[i] - X[i3])
+        yy += yyProduct / (X[i] - X[i2])
+    elif self.nu == 2:
+      yy        = np.zeros_like(xx)
+      yySum     = np.zeros_like(xx)
+      yyProduct = np.ones_like(xx)
+      for i2 in I:
+        if i2 == i: continue
+        yySum.fill(0)
+        for i3 in I:
+          if i3 in (i, i2): continue
+          yyProduct.fill(1)
+          for i4 in I:
+            if i4 in (i, i2, i3): continue
+            yyProduct *= (xx - X[i4]) / (X[i] - X[i4])
+          yySum += yyProduct / (X[i] - X[i3])
+        yy += yySum / (X[i] - X[i2])
+    else:
+      raise NotImplementedError("Higher derivatives are not implemented.")
     
     return yy
   
